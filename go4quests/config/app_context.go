c52b025dc72a36c4473fe077fd08661163cd904b
Igor Grillo Peternella <igor.feq@gmail.com>
2020-04-04 17:09:03 -0300

    Added project path root to app_context.go.

--- go4quests/config/app_context.go
+++ go4quests/config/app_context.go
@@ -3,11 +3,16 @@ package config
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog"
+	"os"
+	"path"
 )
+
+// Root path of the project: GOPATH + src/module
+var ProjectRootPath = path.Join(os.Getenv("GOPATH"), "src/github.com/IgooorGP/go4quests")
 
 // Loads env vars from .env.local if available
 // For production, k8s Config map or other env source should set these variables
-var _ = LoadDotEnvIfAvailable(".env")
+var _ = LoadDotEnvIfAvailable(path.Join(ProjectRootPath, ".env"))
 
 // Host and port of the app
 var SocketBindAddress = GetEnvValueOrUseDefault("GIN_SERVER_TCP_SOCKET_BIND_ADDRESS", "0.0.0.0")
