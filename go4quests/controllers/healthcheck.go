13e856fb9313cb6445933037b202656cc5106c46
Igor Grillo Peternella <igor.feq@gmail.com>
2020-04-04 17:09:42 -0300

    Added healthcheck to infra services folder.

--- go4quests/controllers/healthcheck.go
+++ go4quests/controllers/healthcheck.go
@@ -1,14 +1,14 @@
 package controllers
 
 import (
-	"github.com/IgooorGP/go4quests/go4quests/infra"
+	"github.com/IgooorGP/go4quests/go4quests/infra/services"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 // Controller (interface)
 func HealthCheckController(ctx *gin.Context) {
-	healthCheckApp := infra.NewHealthCheckService()
+	healthCheckApp := services.NewHealthCheckService()
 	healthCheckResult := healthCheckApp.AreExternalServicesHealthy()
 
 	ctx.JSON(http.StatusOK, healthCheckResult)
